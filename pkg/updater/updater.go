67e2d28238113257ac68fc89c74a18bcb6f99ec3
coegle <coegle18@gmail.com>
2026-05-10 12:57:12 +0800

    fix(updater): specify binary filters to find ws executable in archive

--- pkg/updater/updater.go
+++ pkg/updater/updater.go
@@ -12,7 +12,9 @@ const RepoSlug = "coegle/workspace_cli"
 // It returns the latest Release info and any error encountered.
 func Update(currentVersion string) (*selfupdate.Release, error) {
 	// go-selfupdate will automatically use GITHUB_TOKEN if it's in the environment
-	config := selfupdate.Config{}
+	config := selfupdate.Config{
+		Filters: []string{"^ws$|^ws.exe$"}, // Tell selfupdate to look for 'ws' (Linux/macOS) or 'ws.exe' (Windows) inside the archive
+	}
 
 	updater, err := selfupdate.NewUpdater(config)
 	if err != nil {
