cb044232a01c6e03e8c5670c75aab178f68bfb74
coegle <coegle18@gmail.com>
2026-05-10 14:18:47 +0800

    feat(core): dynamically read module version for go install support

--- core/version.go
+++ core/version.go
@@ -1,3 +1,21 @@
 package core
 
+import (
+	"runtime/debug"
+)
+
+// Version is the current version of the application.
+// It will be overwritten by goreleaser's ldflags during release builds.
 var Version = "v0.0.1"
+
+func init() {
+	// If Version is still the default, try to read it from go module build info
+	if Version == "v0.0.1" {
+		if info, ok := debug.ReadBuildInfo(); ok {
+			// When installed via `go install package@version`, Main.Version contains the version
+			if info.Main.Version != "" && info.Main.Version != "(devel)" {
+				Version = info.Main.Version
+			}
+		}
+	}
+}
