6a75f8f3d4e2f2f32174ef46f4c54edaf4f7c007
coegle <coegle18@gmail.com>
2026-05-11 16:36:10 +0800

    feat(cmd): add 'rm' alias for remove command

--- cmd/remove.go
+++ cmd/remove.go
@@ -8,9 +8,10 @@ import (
 )
 
 var removeCmd = &cobra.Command{
-	Use:   "remove <feat_branch_name> <service_repo_name...>",
-	Short: "Remove a service worktree from the feature workspace",
-	Args:  cobra.MinimumNArgs(2),
+	Use:     "remove <feat_branch_name> <service_repo_name...>",
+	Aliases: []string{"rm"},
+	Short:   "Remove a service worktree from the feature workspace",
+	Args:    cobra.MinimumNArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		featBranchName := args[0]
 		serviceRepoNames := args[1:]
